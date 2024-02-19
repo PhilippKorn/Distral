c732e0c51653127f3ca75c0e53ea7f4cfa54ccc4
Philipp Korn <philippkorn@posteo.de>
2024-02-19 16:28:20 +0100

    Update Sheet Dismissing

--- Distral/Views/Questionnaires/PDQ8/PDQView.swift
+++ Distral/Views/Questionnaires/PDQ8/PDQView.swift
@@ -8,84 +8,93 @@
 import SwiftUI
 
 struct PDQView: View {
+    @Environment(\.dismiss) var dismiss
     @ObservedObject var viewModel = PDQ8ViewModel()
     var title: String = ""
     @State private var currentQuestionIndex: Int = 0
     @State private var backToHome: Bool = false
-   
+    
     var body: some View {
         if !viewModel.isQuestionnaireCompleted {
-                questionView
+            questionView
         }
     }
     
     var questionView: some View {
-        VStack(spacing: 20) {
-            HStack {
-                Button("", systemImage: "arrow.backward", action: viewModel.moveThroughQuestionsBackward)
-                    .font(.title)
-                
-                Text("Frage: \(viewModel.questionCounter + 1) / 8")
-                    .font(.system(size: 20))
-                    .padding()
-              
-                Button("", systemImage: "arrow.forward", action: viewModel.moveThroughQuestionsForward)
-                    .font(.title)
-            }
-           
-            ScrollView {
-                Text(viewModel.titles[viewModel.questionCounter] ?? "")
-                    .font(.title3)
-                    .padding()
-            }
-            
-            // Buttons für die Antwortoptionen
-            VStack(spacing: 30) {
-                ForEach(0..<5, id: \.self) { option in
-                    Button(action: {
-                        viewModel.selectOption(option)
-                    }) {
-                        HStack {
-                            Text(answerText(for: option))
-                                .frame(maxWidth: .infinity)
-                            Spacer()
-                            if isOptionSelected(option) {
-                                Image(systemName:"checkmark.circle.fill")
-                                    .foregroundStyle(.blue)
-                            }
-                        }
+        NavigationStack {
+            VStack(spacing: 20) {
+                HStack {
+                    Button("", systemImage: "arrow.backward", action: viewModel.moveThroughQuestionsBackward)
+                        .font(.title)
+                    
+                    Text("Frage: \(viewModel.questionCounter + 1) / 8")
+                        .font(.system(size: 20))
                         .padding()
-                        .background(Color.white)
-                        .foregroundStyle(Color.black)
-                        .overlay (
-                            RoundedRectangle(cornerRadius: 12)
-                                .stroke(isOptionSelected(option) ? Color.blue : Color.white, lineWidth: 3)
-                        )
-                        .cornerRadius(12)
-                    }
-                    .padding(10)
+                    
+                    Button("", systemImage: "arrow.forward", action: viewModel.moveThroughQuestionsForward)
+                        .font(.title)
                 }
                 
-                Button(action: {
-                    viewModel.moveThroughQuestionsForward()
-                }) {
-                    Text("Weiter")
-                        .frame(maxWidth: .infinity)
+                ScrollView {
+                    Text(viewModel.titles[viewModel.questionCounter] ?? "")
+                        .font(.title3)
+                        .padding()
                 }
-                .padding()
-                .background(.blue)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .foregroundStyle(.white)
-                .padding()
+                
+                // Buttons für die Antwortoptionen
+                VStack(spacing: 5) {
+                    ForEach(0..<5, id: \.self) { option in
+                        Button(action: {
+                            viewModel.selectOption(option)
+                        }) {
+                            HStack {
+                                Text(answerText(for: option))
+                                    .frame(maxWidth: .infinity)
+                                    .font(.headline)
+                                Spacer()
+                                if isOptionSelected(option) {
+                                    Image(systemName:"checkmark.circle.fill")
+                                        .foregroundStyle(.blue)
+                                }
+                            }
+                            .padding()
+                            .background(Color.white)
+                            .foregroundStyle(Color.black)
+                            .overlay (
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(isOptionSelected(option) ? Color.blue : Color.white, lineWidth: 5)
+                            )
+                            .cornerRadius(12)
+                        }
+                        .padding(10)
+                    }
+                    
+                    Button(action: {
+                        viewModel.moveThroughQuestionsForward()
+                    }) {
+                        Text("Weiter")
+                            .frame(maxWidth: .infinity)
+                    }
+                    .padding()
+                    .background(.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .foregroundStyle(.white)
+                    .padding()
+                }
+            }
+            .background(.gray.opacity(0.14))
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {dismiss()}) {
+                        Image(systemName: "x.circle")
+                        .foregroundStyle(.gray)
+                    }
+                }
             }
         }
-        .background(.gray.opacity(0.14))
-//        .overlay(
-//            viewModel.showAlertForUnansweredQuestion ? CustomAlertView(showAlert: $viewModel.showAlertForUnansweredQuestion) : nil
-//        )
-//        .onAppear{
-//            print(databaseService.fetchAllPDQ8Results())
-//        }
+        //        .overlay(
+        //            viewModel.showAlertForUnansweredQuestion ? CustomAlertView(showAlert: $viewModel.showAlertForUnansweredQuestion) : nil
+        //        )
     }
     
     func isOptionSelected(_ option: Int) -> Bool {
